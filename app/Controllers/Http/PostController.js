fed149e3fbe9901876cc73e5ef43bb27e8d95b3c
Muhammad Azri <lenovorosak@gmail.com>
2018-06-28 18:50:58 +0800

    update: edit & delete posts

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -3,6 +3,9 @@
 //Bring in model
 const Post= use('App/Models/Post')
 
+//Bring in validator
+const { validate } = use('Validator')
+
 class PostController {
     async index({ view }) {
         // const posts = [
@@ -11,7 +14,7 @@ class PostController {
         //     { title: 'Post three', body: 'This is the body three'}
         // ]
 
-        const posts = await Post.all();
+        const posts = await Post.all()
 
         return view.render('posts.index', {
             title: 'Latest Posts',
@@ -29,6 +32,74 @@ class PostController {
 
     async add ({ view }) {
         return view.render('posts.add')
+    }
+
+    async store ({ request, response, session }) {
+        //Validate input 
+        const validation = await validate(request.all(), {
+            title: 'required|min:3|max:255',
+            body: 'required|min:3'
+        })
+
+        if(validation.fails()){
+            session.withErrors(validation.messages()).flashAll()
+
+            return response.redirect('back')
+        }
+
+        const post = new Post();
+
+        post.title = request.input('title');
+        post.body = request.input('body');
+
+        await post.save()
+
+        session.flash({ notification: 'Post Added!' })
+
+        return response.redirect('/posts')
+    }
+
+    async edit ({ params, view }) {
+        const post = await Post.find(params.id)
+
+        return view.render('posts.edit', {
+            post
+        })
+    }
+
+    async update({ params, request, response, session}) {
+        //Validate input 
+        const validation = await validate(request.all(), {
+            title: 'required|min:3|max:255',
+            body: 'required|min:3'
+        })
+
+        if(validation.fails()){
+            session.withErrors(validation.messages()).flashAll()
+
+            return response.redirect('back')
+        }
+
+        const post = await Post.find(params.id)
+
+        post.title = request.input('title')
+        post.body = request.input('body')
+
+        await post.save()
+
+        session.flash({ notification: 'Post Updated' })
+
+        return response.redirect('/posts')
+    }
+
+    async destroy({ params, session, response  }){
+        const post = await Post.find(params.id)
+
+        await post.delete()
+
+        session.flash({ notification: 'Post Deleted' })
+
+        return response.redirect('/posts')
     }
 }
 
